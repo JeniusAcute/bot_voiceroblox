c8531089a546d6bb55400fd3d294a319aebd7e0f
JeniusAcute <145274224+JeniusAcute@users.noreply.github.com>
2025-06-12 10:00:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ TOKEN = os.environ.get("token")
 #=================================================
 @client.event                                                       #KHOI DONG BOT
 async def on_ready():
-
-    print("Done")
-
-    id_kenh = 1247080044816498711
-    id_server = 1040974953274671205
-    
-    vc = discord.utils.get(client.get_guild(id_server).channels, id = id_kenh)
-    await vc.guild.change_voice_state(channel = vc, self_mute = True, self_deaf = False)
+  
+  print("Done")
+  
+  id_kenh = 1247080044816498711
+  id_server = 1040974953274671205
+  
+  vc = discord.utils.get(client.get_guild(id_server).channels, id = id_kenh)
+  await vc.guild.change_voice_state(channel = vc, self_mute = True, self_deaf = False)
 
 #--------------
 
